5730c5e8e7a9b7a3ef81b476085d1d006af52f31
QuidUtsav <pas081bct048@wrc.edu.np>
2026-02-20 18:29:40 +0545

    fix: sbt scalafmtAll formatted

--- modules/workspace/workspaceClient/src/main/scala/org/llm4s/toolapi/WorkspaceTools.scala
+++ modules/workspace/workspaceClient/src/main/scala/org/llm4s/toolapi/WorkspaceTools.scala
@@ -421,7 +421,7 @@ object WorkspaceTools {
           "Command execution complete - exitCode: " + execResult.exitCode +
             " stdout: " + execResult.stdout.length + " stderr: " + execResult.stderr.length + "b"
         )
-        
+
         ujson.Obj(
           "exit_code" -> execResult.exitCode,
           "stdout"    -> execResult.stdout,
