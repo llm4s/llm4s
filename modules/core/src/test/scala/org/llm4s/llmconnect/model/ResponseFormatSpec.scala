2aa4b85f9baca22366a616822a23649bf210aeda
Piyush-t24 <piyushiitian2023@gmail.com>
2026-02-28 23:54:56 +0530

    Add tests for response_format and skip TTS mock tests when python3 unavailable

---
+++ modules/core/src/test/scala/org/llm4s/llmconnect/model/ResponseFormatSpec.scala
@@ -0,0 +1,54 @@
+package org.llm4s.llmconnect.model
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ResponseFormatSpec extends AnyFlatSpec with Matchers {
+
+  "ResponseFormat.Json" should "be the Json case object" in {
+    ResponseFormat.Json shouldBe ResponseFormat.Json
+  }
+
+  "ResponseFormat.JsonSchema" should "wrap the schema" in {
+    val schema = ujson.Obj("type" -> "object", "properties" -> ujson.Obj("name" -> ujson.Obj("type" -> "string")))
+    val fmt    = ResponseFormat.JsonSchema(schema)
+    fmt.schema shouldBe schema
+  }
+
+  "ResponseFormatMapper.toOpenAIResponseFormat" should "produce json_object for Json" in {
+    val result = ResponseFormatMapper.toOpenAIResponseFormat(ResponseFormat.Json)
+    result shouldBe defined
+    result.get.obj("type").str shouldBe "json_object"
+  }
+
+  it should "produce json_schema structure for JsonSchema" in {
+    val schema = ujson.Obj("type" -> "object", "properties" -> ujson.Obj())
+    val result = ResponseFormatMapper.toOpenAIResponseFormat(ResponseFormat.JsonSchema(schema))
+    result shouldBe defined
+    result.get.obj("type").str shouldBe "json_schema"
+    result.get.obj("json_schema").obj("name").str shouldBe "response"
+    result.get.obj("json_schema").obj("strict").bool shouldBe true
+    result.get.obj("json_schema").obj("schema") shouldBe schema
+  }
+
+  "CompletionOptions" should "have responseFormat None by default" in {
+    CompletionOptions().responseFormat shouldBe None
+  }
+
+  it should "set responseFormat via withResponseFormat" in {
+    val opts = CompletionOptions().withResponseFormat(ResponseFormat.Json)
+    opts.responseFormat shouldBe Some(ResponseFormat.Json)
+  }
+
+  it should "support JsonSchema via withResponseFormat" in {
+    val schema = ujson.Obj("type" -> "object")
+    val opts   = CompletionOptions().withResponseFormat(ResponseFormat.JsonSchema(schema))
+    opts.responseFormat shouldBe Some(ResponseFormat.JsonSchema(schema))
+  }
+
+  it should "preserve other options when using withResponseFormat" in {
+    val opts = CompletionOptions(temperature = 0.5).withResponseFormat(ResponseFormat.Json)
+    opts.temperature shouldBe 0.5
+    opts.responseFormat shouldBe Some(ResponseFormat.Json)
+  }
+}
--- modules/core/src/test/scala/org/llm4s/llmconnect/provider/OpenRouterClientSpec.scala
+++ modules/core/src/test/scala/org/llm4s/llmconnect/provider/OpenRouterClientSpec.scala
@@ -3,7 +3,7 @@ package org.llm4s.llmconnect.provider
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import org.llm4s.llmconnect.config.OpenAIConfig
-import org.llm4s.llmconnect.model.{ CompletionOptions, Conversation, ToolMessage }
+import org.llm4s.llmconnect.model.{ CompletionOptions, Conversation, ResponseFormat, ToolMessage, UserMessage }
 import org.llm4s.metrics.MockMetricsCollector
 
 class OpenRouterClientSpec extends AnyFunSuite with Matchers {
@@ -53,6 +53,39 @@ class OpenRouterClientSpec extends AnyFunSuite with Matchers {
     toolMsg("role").str shouldBe "tool"
     toolMsg("tool_call_id").str shouldBe "call-42"
     toolMsg("content").str shouldBe "tool-output"
+  }
+
+  test("openrouter client adds response_format when ResponseFormat.Json is set") {
+    val client       = new OpenRouterClientTestHelper(testConfig)
+    val conversation = Conversation(Seq(UserMessage("Hello")))
+    val options      = CompletionOptions().withResponseFormat(ResponseFormat.Json)
+
+    val requestBody = client.exposedCreateRequestBody(conversation, options)
+
+    (requestBody.obj should contain).key("response_format")
+    requestBody("response_format")("type").str shouldBe "json_object"
+  }
+
+  test("openrouter client adds response_format with json_schema when ResponseFormat.JsonSchema is set") {
+    val client       = new OpenRouterClientTestHelper(testConfig)
+    val conversation = Conversation(Seq(UserMessage("Hello")))
+    val schema       = ujson.Obj("type" -> "object", "properties" -> ujson.Obj("name" -> ujson.Obj("type" -> "string")))
+    val options      = CompletionOptions().withResponseFormat(ResponseFormat.JsonSchema(schema))
+
+    val requestBody = client.exposedCreateRequestBody(conversation, options)
+
+    (requestBody.obj should contain).key("response_format")
+    requestBody("response_format")("type").str shouldBe "json_schema"
+    (requestBody("response_format")("json_schema").obj should contain).key("schema")
+  }
+
+  test("openrouter client does not add response_format when not set") {
+    val client       = new OpenRouterClientTestHelper(testConfig)
+    val conversation = Conversation(Seq(UserMessage("Hello")))
+
+    val requestBody = client.exposedCreateRequestBody(conversation, CompletionOptions())
+
+    requestBody.obj should not contain key("response_format")
   }
 }
 
